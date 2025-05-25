c1fbc11812f9c7a7e5dfd3b15f7f71000ff6cbf6
Daniil-Pilot <dgrin0414@gmail.com>
2025-05-25 23:00:17 +0300

    Update memory.py

--- bot/memory.py
+++ bot/memory.py
@@ -1,7 +1,4 @@
 from collections import defaultdict
-import asyncio
 
 history_by_user = defaultdict(list)
 users = defaultdict(dict)
-last_message_id_by_user = {}
-user_locks = defaultdict(asyncio.Lock)
